e89023751e428c0206e7a65ce5c4bd9f9ddb9da2
Emran <emran.hadi@gagagugu.com>
2016-06-21 12:56:43 +0600

    Cunt updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    devtool: 'eval',
+    //devtool: 'eval',
     entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:3011',
-        'webpack/hot/only-dev-server',
+        //'react-hot-loader/patch',
+        //'webpack-dev-server/client?http://localhost:3011',
+        //'webpack/hot/only-dev-server',
         './app/app'
     ],
     output: {
@@ -15,7 +15,20 @@ module.exports = {
         publicPath: '/static/'
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_debugger: true
+            },
+            output: {
+                ascii_only: true,
+                comments: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+        })
     ],
     module: {
         loaders: [{
